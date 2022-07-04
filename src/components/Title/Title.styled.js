021d599599d1e5e14db48379b9775abe276d57c6
AlexandrRust <123_00@ukr.net>
2022-07-04 15:53:23 +0300

    finish

--- src/components/Title/Title.styled.js
+++ src/components/Title/Title.styled.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 
 export const Title = styled.h2`
-    font-size: 24px;
-    font-weight: 500;
+    font-size: 28px;
+    font-weight: 700;
+    margin: 0;
 `;
